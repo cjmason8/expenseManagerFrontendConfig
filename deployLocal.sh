7d12222cdeccbfeaf4db8a77fddc7078393c0a2d
Chris Mason <cjmason8@gmail.com>
2019-09-27 13:28:30 +1000

    add new local

--- deployLocal.sh
+++ deployLocal.sh
@@ -7,6 +7,8 @@ if [ $COMPUTER_NAME = "maso97" ]; then
     ./deploy.sh 4B12F965799E4FCF9399 mQtNkKMTmjtF4rNH5AEF239ebcbH3tawx1Dc5C3c http://localhost:8080/v2-beta/projects/1a5 lcl
 elif [ $COMPUTER_NAME = "maso89" ]; then
     ./deploy.sh 0C0636EC95524674AABF YVLbqbL14gcq2yQPJvbwz3MeJK5UAWNgKoYe9UCL http://localhost:8080/v2-beta/projects/1a5 lcl
+elif [ $COMPUTER_NAME = "maso71" ]; then
+    ./deploy.sh 389DF31EAD49512C006F BKz8WH8n7rR37vpXKfCSZTKrAqY6TLQ5BS6eKCA6 http://localhost:8080/v2-beta/projects/1a5 lcl
 else
     ./deploy.sh C3EB1019A856653D3D7A WcF7KbNgrUStCYryH7YWKPvKf3VkfV3rnvtb4QnF http://localhost:8080/v2-beta/projects/1a5 lcl
 fi

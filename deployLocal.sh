9d847f9e4380ddcfc063818fa746128078496dc8
Chris Mason <cjmason8@gmail.com>
2020-10-16 15:40:19 +1100

    remove source

--- deployLocal.sh
+++ deployLocal.sh
@@ -11,6 +11,8 @@ elif [ $COMPUTER_NAME = "maso71" ]; then
     ./deploy.sh 389DF31EAD49512C006F BKz8WH8n7rR37vpXKfCSZTKrAqY6TLQ5BS6eKCA6 http://localhost:8080/v2-beta/projects/1a5 lcl
 elif [ $COMPUTER_NAME = "maso86" ]; then
     ./deploy.sh  9842480944A9D72541B6 XV4q4nTtrScVXMcQtVbuCtZnznU5Tu1ikKdu9dyR http://localhost:8080/v2-beta/projects/1a5 lcl
+elif [ $COMPUTER_NAME = "maso11" ]; then
+    ./deploy.sh 1E393C47576DA5E03279 mNEuCu2p6Ju5sjgYrqRYJFfiagXpc5N6egVbeAnc http://localhost:8080/v2-beta/projects/1a5 lcl
 else
     ./deploy.sh C3EB1019A856653D3D7A WcF7KbNgrUStCYryH7YWKPvKf3VkfV3rnvtb4QnF http://localhost:8080/v2-beta/projects/1a5 lcl
 fi
